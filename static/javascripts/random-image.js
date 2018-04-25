74cecc73489b3719227ee3862fbbe8605e27fa07
Mina Gaid <mina.gaid@ibm.com>
2018-04-25 13:03:39 +0100

    new image

--- static/javascripts/random-image.js
+++ static/javascripts/random-image.js
@@ -3,6 +3,7 @@ theImages[0] = '../static/images/index-1.png'
 theImages[1] = '../static/images/index-2.png'
 theImages[2] = '../static/images/index-3.png'
 theImages[3] = '../static/images/index-4.png'
+theImages[4] = '../static/images/index-5.png'
 var j = 0
 var p = theImages.length;
 var preBuffer = new Array()
@@ -23,6 +24,8 @@ function showImage() {
  } else if (whichImage == 3) {
   document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="55%" height="70px">');
  } else if (whichImage == 4) {
+  document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="55%" height="70px">');
+ } else if (whichImage == 5) {
   document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="55%" height="70px">');
  }
 
