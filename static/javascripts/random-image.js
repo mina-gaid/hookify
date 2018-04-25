1a7c6bd5f4be24b78be59a424de7f2f4d31c03ba
Mina Gaid <mina.gaid@ibm.com>
2018-04-25 12:09:11 +0100

    image fitting

--- static/javascripts/random-image.js
+++ static/javascripts/random-image.js
@@ -15,15 +15,15 @@ var whichImage = Math.round(Math.random() * (p - 1));
 
 function showImage() {
  if (whichImage == 0) {
-  document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="65%" height="70px">');
+  document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="55%" height="70px">');
  } else if (whichImage == 1) {
-  document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="65%" height="70px">');
+  document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="55%" height="70px">');
  } else if (whichImage == 2) {
-  document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="65%" height="70px">');
+  document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="55%" height="70px">');
  } else if (whichImage == 3) {
-  document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="65%" height="70px">');
+  document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="55%" height="70px">');
  } else if (whichImage == 4) {
-  document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="65%" height="70px">');
+  document.write('<img src="' + theImages[whichImage] + '" class="img-fluid" border=0 width="55%" height="70px">');
  }
 
 }
